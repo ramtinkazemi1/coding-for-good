dd8200b59676720c695e1a39e38a80bb9907028d
Ramtin Kazemi <ramkazemi@yahoo.com>
2023-10-02 12:25:34 -0700

    Time: 413 ms (55.42%) | Memory: 17.5 MB (51.33%) - LeetSync

---
+++ 55-jump-game/jump-game.py
@@ -0,0 +1,14 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        
+        farthest = 0  # Initialize the farthest position to 0
+
+        for i in range(len(nums)):
+            if i > farthest:
+                return False  # If the current position is unreachable, return False
+            farthest = max(farthest, i + nums[i])  # Update the farthest reachable position
+
+        return farthest >= len(nums) - 1
+
+
+        
