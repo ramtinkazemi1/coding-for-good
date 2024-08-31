4e97c62275f9af853946b41e0f852b504428f807
Ramtin Kazemi <ramkazemi@yahoo.com>
2024-08-31 00:21:17 -0700

    Time: 532 ms (30.83%) | Memory: 29.9 MB (25.48%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,20 +1,21 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        
-        ans = 0
+        #height = [1,8,6,2,5,4,8,3,7]   ans = 49
+        # two pointers
 
         left = 0
         right = len(height)-1
+        max_area = 0
+        # area = min(p2 , p1) *  (p2 - p1)
+        # area formula =  max(area, ((min(height[right] , height[left])) *  (right - left))
 
-        while left <= right:
+        while left < right:
+            current_area = min(height[left], height[right]) * (right - left)
+            max_area = max(max_area, current_area)
 
-            ans = max(ans, min(height[left],height[right])*abs(right-left))
-            print(left, height[left],right, height[right] , abs(right-left))
-            
-            #important if-else block
-            if height[left]<height[right]:
-                left+=1
+            if height[left] < height[right]:
+                left += 1
             else:
-                right-=1
-
-        return ans
+                right -= 1
+        
+        return max_area
